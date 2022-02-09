d4ebe1d75916a942446327950e9357e2450dc2be
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-02-09 09:47:00 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,6 +2,26 @@
 #include <iostream>
 using namespace std;
 
+const float MAX_TEMPERATURE = 45.0;
+const float MIN_TEMPERATURE = 0.0;
+const float MAX_SOC = 80.0;
+const float MIN_SOC = 20.0;
+const float MAX_CHARGERATE = 0.8;
+const float MIN_CHARGERATE = 0.0;
+
+int checkValueinRange(const float maxValue, const float minValue, const float ValuetoCheck)
+{
+  if (ValuetoCheck < minValue)
+  {
+    return -1 ;
+  }
+  else if (ValuetoCheck > maxValue)
+  {
+    return 0;
+  }
+  return 1;
+}
+
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
   if(temperature < 0 || temperature > 45) {
     cout << "Temperature out of range!\n";
