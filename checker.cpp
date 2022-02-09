dcb427408c1cd478809b618463bee6a2ea9e63a5
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-02-09 10:01:26 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -9,34 +9,28 @@ const float MIN_SOC = 20.0;
 const float MAX_CHARGERATE = 0.8;
 const float MIN_CHARGERATE = 0.0;
 
-int checkValueinRange(const float maxValue, const float minValue, const float ValuetoCheck)
+ bool checkValueinRange(const float maxValue, const float minValue, const float ValuetoCheck)
 {
-  if (ValuetoCheck < minValue)
+  if (ValuetoCheck < minValue || ValuetoCheck > maxValue )
   {
-    return -1 ;
-  }
-  else if (ValuetoCheck > maxValue)
-  {
-    return 0;
+    return 0 ;
   }
   return 1;
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+  bool checkTemperature = false;
+  bool checkSOC = false;
+  bool checkChargeRate = false;
+  
+  checkTemperature = checkValueinRange(MAX_TEMPERATURE, MIN_TEMPERATURE, temperature);
+  checkSOC = checkValueinRange(MAX_SOC, MIN_SOC, soc);
+  checkChargeRate = checkValueinRange(MAX_CHARGERATE, MIN_CHARGERATE, chargeRate);
+  
+  return checkTemperature && checkSOC && checkChargeRate;
 }
 
-int main() {
+  int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
 }
